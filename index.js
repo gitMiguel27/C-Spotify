434d3c682baa1c43223f6f8dcb44c9e38003c7b0
Miguel Nazario <mnazario35@gmail.com>
2022-02-15 10:28:52 -0500

    accessed each artists name and image

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 //     response.end();
 //   }).listen(8888);
 
-var accessToken = 'BQC9KV7_gN7rxDH-BdNufDNUq4DQ0gq0k5PUs1UFwtUGvDGM_77oQRd0lXDy-goNYXcEiUhVxab11gUpKvpF78FfyDlsYSTegdUxBGLIK-f-EdFfQ22oG5CpFW7vuahVH8POzmZDWHeHgLg';
+var accessToken = 'BQAPcKN43KHJ12ZVjHKWOiypjvVPvSqpkWh-Pq8OgLWbKAXo9KsRcSHW-HSvq3Wy20lM_vi9uZ8jZEaHLxIl5HfbhBvboeIhMo3triRA9dVbKX1c2SvA5y3tVgs5BXZT0YHFSqgK3sXlXqY';
 
 fetch('https://api.spotify.com/v1/search?q=Bad%20Bunny&type=artist', {
   method: 'GET',
@@ -18,18 +18,45 @@ fetch('https://api.spotify.com/v1/search?q=Bad%20Bunny&type=artist', {
     'Authorization': 'Bearer ' + accessToken
   }
 })
-.then(response => response.json())
-.then(artists => {
-  console.log(artists);
-  renderImage(artists);
-})
+// .then(response => response.json())
+// .then(artistsData => {
+//   console.log(artistsData);
+//   // console.log(artistsData.artists.items)
+//   let realBadBunny = artistsData.artists.items[0];
+//   renderImage(realBadBunny);
+// })
 
-function renderImage(artists) {
+// function renderImage(realBadBunny) {
+//   let div = document.createElement('div');
+//   let img = document.createElement('img');
+//   // debugger;
+//   img.src = realBadBunny.images[0].url;
+
+//   document.body.append(div);
+//   div.appendChild(img);
+// }
+
+.then(response => response.json())
+.then(artistsData => {
+  // console.log(artistsData);
+  // console.log(artistsData.artists);
+  // console.log(artistsData.artists.items);
+  let items = artistsData.artists.items;
+  for(let i = 0; i < 3; i++) {
+    renderEachArtistsData(items[i]);
+  };
+});
+
+function renderEachArtistsData(element) {
+  console.log(element.name, element.images[0]);
   let div = document.createElement('div');
   let img = document.createElement('img');
+  let name = document.createElement('p');
+  name.textContent = element.name;
   // debugger;
-  img.src = artists.items[0].images[0].url;
+  img.src = element.images[0].url;
 
   document.body.append(div);
+  div.appendChild(name);
   div.appendChild(img);
 }
